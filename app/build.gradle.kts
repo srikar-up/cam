13b185f035d156654db6b9a74ccbcd099d542c5d
Srikar-up <srikarsensai@gmail.com>
2026-07-11 16:53:16 +0530

    feat: opt-in to ExperimentalMaterial3Api in Kotlin compiler arguments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs = freeCompilerArgs + listOf("-opt-in=androidx.compose.material3.ExperimentalMaterial3Api")
     }
     buildFeatures {
         compose = true
